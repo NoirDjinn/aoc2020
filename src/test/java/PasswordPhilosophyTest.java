b86d1659a011fe2d82f7cf4ba5af397629b9b728
Dmitry Konyshev <dk@noirdjinn.dev>
2020-12-05 13:19:27 +0300

    Format code in tests

--- src/test/java/PasswordPhilosophyTest.java
+++ src/test/java/PasswordPhilosophyTest.java
@@ -1,22 +1,24 @@
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
 public class PasswordPhilosophyTest {
-    private static PasswordPhilosophy pp;
 
-    public PasswordPhilosophyTest() {
-        pp = new PasswordPhilosophy();
-    }
+  private static PasswordPhilosophy pp;
 
-    @Test
-    void day2_part1_test() {
-        long res = pp.countCorrectPasswords();
-        assertEquals(416, res);
-    }
+  public PasswordPhilosophyTest() {
+    pp = new PasswordPhilosophy();
+  }
 
-    @Test
-    void day2_part2_test() {
-        long res = pp.countValidPasswords();
-        assertEquals(688, res);
-    }
+  @Test
+  void day2_part1_test() {
+    long res = pp.countCorrectPasswords();
+    assertEquals(416, res);
+  }
+
+  @Test
+  void day2_part2_test() {
+    long res = pp.countValidPasswords();
+    assertEquals(688, res);
+  }
 }
--- src/test/java/ReportRepairTest.java
+++ src/test/java/ReportRepairTest.java
@@ -1,23 +1,24 @@
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class ReportRepairTest {
 
-    private static ReportRepair rr;
+  private static ReportRepair rr;
 
-    public ReportRepairTest() {
-        rr = new ReportRepair();
-    }
+  public ReportRepairTest() {
+    rr = new ReportRepair();
+  }
 
-    @Test
-    void day1_part1_test() {
-        long res = rr.FindEntries();
-        assertEquals(445536, res);
-    }
+  @Test
+  void day1_part1_test() {
+    long res = rr.FindEntries();
+    assertEquals(445536, res);
+  }
 
-    @Test
-    void day1_part2_test() {
-        long res = rr.GetStarfishCoin();
-        assertEquals(138688160, res);
-    }
+  @Test
+  void day1_part2_test() {
+    long res = rr.GetStarfishCoin();
+    assertEquals(138688160, res);
+  }
 }
